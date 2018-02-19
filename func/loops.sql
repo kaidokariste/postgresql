7b526a6bc53e20ce60e8789a8c23c6a4f49f09d3
Kaido Kariste <kaido.kariste@bigbank.ee>
2018-02-19 14:45:59 +0200

    Array looping

--- func/loops.sql
+++ func/loops.sql
@@ -47,8 +47,7 @@ BEGIN
 END;
 $$
 
--- The while loop
-
+--While loop
 DO LANGUAGE plpgsql
 $$
 DECLARE
@@ -70,8 +69,7 @@ END;
 $$
 
 -- Using array to make conditional updating
-
-DO  LANGUAGE plpgsql $$
+DO LANGUAGE plpgsql $$
 DECLARE
   names_array TEXT [] := ('{Kaido, Hannes, Mihkel, Vaiko}');
   first_name  TEXT;
@@ -83,10 +81,29 @@ BEGIN
 END
 $$;
 
---
-
-
-
-select round((random()*100)::numeric,0)
-
-select round(2.25, 1)
+--Using array looping and conditional if-else statements
+DO LANGUAGE plpgsql $$
+DECLARE
+  arCountry TEXT [] := ('{Estonia, France, Spain, Italy}');
+  arCapital TEXT [] := ('{Madrid, Tallinn, Paris, Rome}');
+  vCountry  VARCHAR;
+  vCapital  VARCHAR;
+BEGIN
+  FOREACH vCountry IN ARRAY arCountry
+  LOOP
+    RAISE INFO 'Country is %', vCountry;
+    FOREACH vCapital IN ARRAY arCapital
+    LOOP
+      IF vCountry = 'Estonia' AND vCapital = 'Tallinn'
+      THEN
+        RAISE INFO '% is capital of %', vCapital, vCountry;
+      ELSEIF vCountry = 'France' AND vCapital = 'Paris'
+        THEN
+          RAISE INFO '% is capital of %', vCapital, vCountry;
+      ELSE
+        RAISE INFO 'Relation between % and % is unknown', vCapital, vCountry;
+      END IF;
+    END LOOP;
+  END LOOP;
+END
+$$;
