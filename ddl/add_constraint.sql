d8a58bec3eba0f9bda33ae2f33cfbb7aa9c7e170
kaidokariste <kaidokariste@users.noreply.github.com>
2019-07-11 17:18:01 +0300

    Update add_constraint.sql

--- ddl/add_constraint.sql
+++ ddl/add_constraint.sql
@@ -4,4 +4,5 @@ ALTER TABLE my_table ADD CHECK (name_column <> '');
 -- Add unique constraint to my table, named my_constraint_name to column column_product_no
 ALTER TABLE my_table ADD CONSTRAINT my_constraint_name UNIQUE (column_product_no);
 
-
+-- Add column NOT NULL constraint
+ALTER TABLE myTable ALTER COLUMN name_column SET NOT NULL;
