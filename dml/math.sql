0efe053b4528b4b7aa27721dbc9388760776b3ec
kaidokariste <kaidokariste@users.noreply.github.com>
2019-09-05 11:43:28 +0300

    Using filter to calculate percentage

---
+++ dml/math.sql
@@ -0,0 +1,2 @@
+-- Calculate the percentage of successful loads compared to all loads
+select round(count(1) filter (where load_status = 'SUCCESS') / count(1)::DECIMAL * 100,2) from mySchema.myDataLoad
