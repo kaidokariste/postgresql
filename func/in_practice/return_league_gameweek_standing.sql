9c30c59799891d0b2813740a7066cf0a5560ae67
kaido.kariste <kaido.kariste@bigbank.ee>
2020-01-13 09:37:20 +0200

    SOLUTION 2 for gameweek overview

--- func/in_practice/return_league_gameweek_standing.sql
+++ func/in_practice/return_league_gameweek_standing.sql
@@ -21,33 +21,122 @@ insert into football_league(team_name, gameweek_number, points, goal_difference)
 
 --  
 
-CREATE OR REPLACE FUNCTION league_standing(par_gw integer)
-RETURNS TABLE(place bigint, team_name varchar, gameweek_number integer, points integer, goal_difference integer, change_from_previous text) 
+CREATE OR REPLACE FUNCTION league_standing(par_gw INTEGER)
+    RETURNS TABLE
+            (
+                place                BIGINT,
+                team_name            VARCHAR,
+                gameweek_number      INTEGER,
+                points               INTEGER,
+                goal_difference      INTEGER,
+                change_from_previous TEXT
+            )
 AS
 $$
-with current_gameweek as (
-select 
-team_name, gameweek_number, points, goal_difference, 
-ROW_NUMBER() OVER(PARTITION BY gameweek_number ORDER BY points DESC, goal_difference DESC ) as position
-from football_league fl
-where gameweek_number = par_gw
-),
-previous_gameweek as (
-select 
-team_name, gameweek_number, points, goal_difference, 
-ROW_NUMBER() OVER(PARTITION BY gameweek_number ORDER BY points DESC, goal_difference DESC ) as position
-from football_league fl
-where gameweek_number = (select max(gameweek_number) -1 from current_gameweek)
-)
-select cg.position, cg.team_name, cg.gameweek_number, cg.points, cg.goal_difference,   
-	case when (pg.position - cg.position) > 0 then '+'|| (pg.position - cg.position)
-	else coalesce((pg.position - cg.position), 0) ::text
-	end as change_from_previous  
-from current_gameweek cg
-left join previous_gameweek pg on pg.team_name = cg.team_name
-order by cg.position asc;
- $$ LANGUAGE sql
+WITH current_gameweek AS (
+    SELECT   team_name,
+             gameweek_number,
+             points,
+             goal_difference,
+             ROW_NUMBER()
+             OVER (PARTITION BY gameweek_number ORDER BY points DESC, goal_difference DESC ) AS position
+      FROM football_league fl
+      WHERE gameweek_number = par_gw),
+
+     previous_gameweek AS (
+         SELECT team_name,
+                gameweek_number,
+                points,
+                goal_difference,
+                ROW_NUMBER() OVER (PARTITION BY gameweek_number ORDER BY points DESC, goal_difference DESC ) AS position
+       FROM football_league fl
+       WHERE gameweek_number = (SELECT max(gameweek_number) - 1 FROM current_gameweek))
+SELECT cg.position,
+       cg.team_name,
+       cg.gameweek_number,
+       cg.points,
+       cg.goal_difference,
+       CASE
+           WHEN (pg.position - cg.position) > 0 THEN '+' || (pg.position - cg.position)
+           ELSE coalesce((pg.position - cg.position), 0) ::TEXT END AS change_from_previous
+FROM current_gameweek cg
+         LEFT JOIN previous_gameweek pg ON pg.team_name = cg.team_name
+ORDER BY cg.position ASC;
+$$ LANGUAGE SQL;
  
+ select * from league_standing(1);
+ select * from league_standing(2);
  select * from league_standing(3);
+
+-- SOLUTION2 2 Using lag and temporary tables
+-- Function for returning calculated results:
+DROP FUNCTION IF EXISTS footbal_results(INTEGER);
+
+CREATE OR REPLACE FUNCTION footbal_results(
+    game_week INTEGER
+)
+  RETURNS TABLE (
+    place BIGINT,
+    team_name VARCHAR,
+    gameweek_number INT,
+    points INT,
+    goal_difference INT,
+    change_from_previous TEXT
+  ) AS
+$BODY$
+
+DECLARE
+
+BEGIN
+
+  CREATE TEMPORARY TABLE temp_1_position ON COMMIT DROP AS
+    SELECT
+      t1.*,
+      row_number() OVER (PARTITION BY t1.gameweek_number ORDER BY t1.points DESC, t1.goal_difference DESC) AS position
+    FROM footbal t1;
+
+
+  CREATE TEMPORARY TABLE temp_2_previous_position ON COMMIT DROP AS
+    SELECT
+      t1.*,
+      coalesce( (lag(position, 1) OVER (PARTITION BY t1.team_name ORDER BY t1.gameweek_number) )::TEXT, 'None') AS previous_position
+    FROM temp_1_position t1;
+
+  CREATE TEMPORARY TABLE temp_3_change_from_previous ON COMMIT DROP AS
+  SELECT
+    t1.*,
+    CASE WHEN t1.gameweek_number = 1 THEN t1.position::TEXT -- Week 1 - Exception
+    ELSE
+      ( CASE WHEN t1.previous_position::INT - position::INT > 0
+              THEN '+' || (t1.previous_position::INT - position::INT)::TEXT
+             ELSE (t1.previous_position::INT - position::INT)::TEXT END )
+    END AS change_from_previous
+  FROM temp_2_previous_position t1;
+
+  CREATE TEMPORARY TABLE temp_4_final_dataset ON COMMIT DROP AS
+     SELECT
+      t1.position AS place,
+      t1.team_name,
+      t1.gameweek_number,
+      t1.points,
+      t1.goal_difference,
+      t1.change_from_previous
+     FROM temp_3_change_from_previous t1
+     WHERE t1.gameweek_number = game_week
+     ORDER BY t1.position;
+
+  RETURN QUERY EXECUTE
+  $$
+     SELECT place, team_name, gameweek_number, points, goal_difference, change_from_previous FROM temp_4_final_dataset;
+  $$;
+
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+-- Testing
+SELECT * FROM footbal_results(1);
+SELECT * FROM footbal_results(2);
+SELECT * FROM footbal_results(3);
 
 
