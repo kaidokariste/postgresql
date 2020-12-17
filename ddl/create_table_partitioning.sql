fe84581ddc09e5c041a9cba7a0c04e75c30a05dc
kaido.kariste <kaido.kariste@bigbank.ee>
2020-12-17 22:34:53 +0200

    Native partitioning pg12

--- ddl/create_table_partitioning.sql
+++ ddl/create_table_partitioning.sql
@@ -1,4 +1,36 @@
-﻿-- Table inheritance
+﻿-- After postgres 10, partitioning become more native. Lets look the same example.
+-- Most popular are range partition and list partition. Range partition is used
+-- mostly for dates. List is used for partitioning by common value (like in example)
+
+CREATE TABLE "kaido.kariste".estonia_football_club (
+  club_name VARCHAR(50) NOT NULL,
+  location VARCHAR,
+  who_added VARCHAR(30) DEFAULT current_user
+) PARTITION BY LIST(location);
+
+CREATE TABLE "kaido.kariste".club_tallinn
+    PARTITION OF "kaido.kariste".estonia_football_club FOR VALUES IN ('TALLINN');
+
+CREATE TABLE "kaido.kariste".club_tartu
+    PARTITION OF "kaido.kariste".estonia_football_club FOR VALUES IN ('TARTU');
+
+CREATE TABLE "kaido.kariste".club_other
+    PARTITION OF "kaido.kariste".estonia_football_club FOR VALUES IN ('OTHER');
+
+INSERT INTO "kaido.kariste".estonia_football_club (club_name, location) VALUES
+  ('Tallinna FC Flora','TALLINN'),
+  ('Tallinna FC Levadia','TALLINN'),
+  ('Nõmme Kalju FC','TALLINN'),
+  ('FCI Tallinn','TALLINN');
+
+-- Like it has seen here. You can insert data to parent and Postgres is now
+-- able to
+select * from "kaido.kariste".estonia_football_club;
+select * from "kaido.kariste".club_tallinn;
+select * from "kaido.kariste".club_tartu;
+
+----------------------------------------------------------------------
+-- Table inheritance, used before postgres 10
 CREATE TABLE myschema.premium_liiga_club (
   club_name VARCHAR(50) NOT NULL,
   who_added VARCHAR(30) DEFAULT current_user
